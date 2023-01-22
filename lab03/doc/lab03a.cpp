8a5121b5bc839d26d348e4cc9a6663b42e20efa3
DariusSudeiko <Darius.Sudeiko@cs.khpi.edu.ua>
2023-01-22 18:02:51 +0100

    Start of lab05

---
+++ lab03/doc/lab03a.cpp
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A three-digit number is given. Calculate the value of the second digit in the number.
+# For example,𝑥 = 456 → 𝑦 = 5$.
+
+read -r -p "enter a three digit number" num
+
+	num="$(( num / 10 ))"
+	digit="$((num % 10))"
+	printf "the second digit is: %d\n" "${digit}"
