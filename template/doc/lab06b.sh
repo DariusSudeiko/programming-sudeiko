0e78a96586dbec465547a60b016521ce214aa04f
DariusSudeiko <Darius.Sudeiko@cs.khpi.edu.ua>
2023-01-23 17:53:29 +0100

    First task is done

---
+++ template/doc/lab06b.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Determine the position of the number with the minimum value in a three-digit number.
+# For example, if𝑥 = 413, the minimum value is 1, which is in the 2nd position.
+
+read -r -p "Please enter a three digit number: " num
+
